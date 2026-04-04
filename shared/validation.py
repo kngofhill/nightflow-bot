e437cf72d26f8c57529622e3b085d8fdc4a87a23
kngofhill <boymirzayevismoiljon62@gmail.com>
2026-04-04 23:26:14 +0500

    full

--- shared/validation.py
+++ shared/validation.py
@@ -4,7 +4,7 @@ from datetime import time, date
 import re
 
 class TimeWindow(BaseModel):
-    time: str = Field(..., regex=r'^([01]?[0-9]|2[0-3]):[0-5][0-9]$', description="Time in HH:MM format")
+    time: str = Field(..., pattern=r'^([01]?[0-9]|2[0-3]):[0-5][0-9]$', description="Time in HH:MM format")
     message: Optional[str] = Field(None, max_length=100)
 
 class ConstantScheduleCreate(BaseModel):
